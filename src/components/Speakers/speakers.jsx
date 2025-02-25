bb3844205c5e0f9e28ea0dc0c7dfb8148862653d
KhushiMittal30 <khushimittal771@gmail.com>
2025-02-26 01:48:52 +0530

    error resolved

--- src/components/Speakers/speakers.jsx
+++ src/components/Speakers/speakers.jsx
@@ -12,10 +12,10 @@ const speakers = () => {
   <article>
     <div class="article-wrapper">
       <figure>
-        <img src="https://picsum.photos/id/1011/800/450" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/05/58/20/28/1000_F_558202843_OKjb5BSDd2vkRloCScoTH9wdjTglnoFk.jpg" alt="" />
       </figure>
       <div class="article-body">
-        <h2 className='ubuntu-regular'>DUA LIPA</h2>
+        <h2 className='ubuntu-regular'>Yet to be revealed</h2>
         <p>
           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
         </p>
@@ -31,10 +31,10 @@ const speakers = () => {
   <article>
     <div class="article-wrapper">
       <figure>
-        <img src="https://picsum.photos/id/1011/800/450" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/05/58/20/28/1000_F_558202843_OKjb5BSDd2vkRloCScoTH9wdjTglnoFk.jpg" alt="" />
       </figure>
       <div class="article-body">
-        <h2 className='ubuntu-regular'>DUA LIPA</h2>
+        <h2 className='ubuntu-regular'>Yet to be revealed</h2>
         <p>
           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
         </p>
@@ -50,10 +50,10 @@ const speakers = () => {
   <article>
     <div class="article-wrapper">
       <figure>
-        <img src="https://picsum.photos/id/1005/800/450" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/05/58/20/28/1000_F_558202843_OKjb5BSDd2vkRloCScoTH9wdjTglnoFk.jpg" alt="" />
       </figure>
       <div class="article-body">
-        <h2 className='ubuntu-regular'>DILJIT DOSANJH</h2>
+        <h2 className='ubuntu-regular'>Yet to be revealed</h2>
         <p>
           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
         </p>
@@ -69,10 +69,10 @@ const speakers = () => {
   <article>
     <div class="article-wrapper">
       <figure>
-        <img src="https://picsum.photos/id/1005/800/450" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/05/58/20/28/1000_F_558202843_OKjb5BSDd2vkRloCScoTH9wdjTglnoFk.jpg" alt="" />
       </figure>
       <div class="article-body">
-        <h2 className='ubuntu-regular'>DILJIT DOSANJH</h2>
+        <h2 className='ubuntu-regular'>Yet to be revealed</h2>
         <p>
           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
         </p>
@@ -88,10 +88,10 @@ const speakers = () => {
   <article>
     <div class="article-wrapper">
       <figure>
-        <img src="https://picsum.photos/id/1005/800/450" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/05/58/20/28/1000_F_558202843_OKjb5BSDd2vkRloCScoTH9wdjTglnoFk.jpg" alt="" />
       </figure>
       <div class="article-body">
-        <h2 className='ubuntu-regular'>DILJIT DOSANJH</h2>
+        <h2 className='ubuntu-regular'>Yet to be revealed</h2>
         <p>
           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
         </p>
@@ -108,14 +108,14 @@ const speakers = () => {
 
     <div class="article-wrapper">
       <figure>
-        <img src="https://picsum.photos/id/103/800/450" alt="" />
+        <img src="https://as1.ftcdn.net/v2/jpg/05/58/20/28/1000_F_558202843_OKjb5BSDd2vkRloCScoTH9wdjTglnoFk.jpg" alt="" />
       </figure>
       <div class="article-body">
-        <h2 className='ubuntu-regular'>ARJIT SINGH</h2>
+        <h2 className='ubuntu-regular'>Yet to be revealed</h2>
         <p>
           Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
         </p>
-        <a href="#" class="read-more">
+        <a href="https://www.linkedin.com/in/khushi-mittal-21b3ba246/" class="read-more">
           LinkedIn 
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
